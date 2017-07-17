e4e0e8da02a8917c9290a6e2eb40a8f345514ca8
James Chen <jamescchen53@gmail.com>
2017-07-17 15:41:51 -0700

    express server created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var bot_token = process.env.SLACK_BOT_TOKEN || ''; 
+var bot_token = process.env.BOT_OAUTH_TOKEN || ''; 
+var express = require('express')
 
+var app = express();
 
 var rtm = new RtmClient(bot_token);
 
@@ -12,7 +14,7 @@ let channel;
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for (const c of rtmStartData.channels) {
-    if (c.is_member && c.name ==='general') { channel = c.id } //if the bot is a member of the general channel
+    if (c.is_member && c.name ==='scheduler') { channel = c.id } //if the bot is a member of the general channel
   }
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
@@ -28,3 +30,24 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 });
 
 rtm.start();
+
+app.get('/', function(req, res){
+	console.log('hello!!')
+	res.send("HI")
+})
+
+/*
+Create a POST route in the Express server and configure Slack 
+to send the response of Interactive Messages to this route. 
+Verify that this route is called when a user click on an 
+interactive message.
+*/
+
+app.post('/', function(req,res){
+	
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
