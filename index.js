28a0864a76be010329c9634726882b5f6083732c
James Chen <jamescchen53@gmail.com>
2017-07-17 17:53:10 -0700

    got a lot more stuff working but not all; should be close to having conversation flow on slack

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var { RtmClient, WebClient, CLIENT_EVENTS, RTM_EVENTS } = require('@slack/client');
 var bot_token = process.env.BOT_OAUTH_TOKEN || ''; 
-var express = require('express')
-
+var express = require('express');
+var axios = require('axios');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
 var rtm = new RtmClient(bot_token);
+var web = new WebClient(bot_token);
 
 let channel;
-
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
@@ -19,22 +20,83 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
+
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   rtm.sendMessage("Hello! I'm going to rip your guts out and smear it across your face!", channel);
 });
 
+// curl 'https://api.api.ai/api/query?v=20150910&query=remind%20me%20to%20take%20the%20kids%20to%20get%20washed&lang=en&sessionId=5df54d47-3d0f-4951-9365-8eda5765203a&timezone=2017-07-17T17:44:14-0700' -H 'Authorization:Bearer 451d79d162e8407982aa7896b4a24faf'
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(msg) {
+		var dm = rtm.dataStore.getDMByUserId(msg.user);
+		if(!dm || dm.id !== msg.channel || msg.type !== 'message'){
+			console.log('NOT DM, IGNORING');
+			return; //message not sent to DM, ignoring
+		}
+	rtm.sendMessage(msg.text, msg.channel)
+	axios.get('https://api.api.ai/api/query', {
+		params: {
+			v: '20150910',
+			lang: 'en',
+			timezone: '2017-07-17T17:44:14-0700',
+			query: msg.text,
+			sessionId: msg.user
+		}, 
+		headers: {
+			Authorization: 'Bearer 451d79d162e8407982aa7896b4a24faf'
+		}
+	}).then(({ data }) => {
+		if(data.result.actionIncomplete){ //need subject and date to be complete
+			rtm.sendMesasge(data.result.fulfillment.speech, msg.channel)
+		} else {
+			console.log("ACTION IS COMPLETE", data.result.parameters) //when you enable interactive messages it requires a url from ngrok, make path that makes sense
+			web.chat.postMessage(msg.channel, 'Creating reminder to '${data.result.parameters.description}' on '${data.result.parameters.date}, {
+				"attachments": [
+				{
+					"actions": [
+						"name": "confirm" //FINISH WRITING IN THE CONFIRM/CANCEL BUTTONS!!
+					]
+				}
+				]
+			})
+		}
+	}).catch((err) => {
+		console.log('ERROR!:', err)
+	})
 });
 
 rtm.start();
 
-app.get('/', function(req, res){
-	console.log('hello!!')
-	res.send("HI")
+app.post('/slack/interact', function(req, res){
+	//need to set up bodyparser
+	var payload = JSON.parse(req.body.payload);
+	if(payload.actions[0].value === 'true') {
+		res.send('will do!! :jayisgr8:')
+	} else {
+		res.send('Cancelled')
+	}
 })
+
+app.get('/', function(req, res){
+	var oauth2Client = new OAuth2(
+		process.env.GOOGLE_CLIENT_ID,
+		process.env.GOOGLE_CLIENT_SECRET,
+		process.env.DOMAIN + '/connect/callback'
+	);
+
+	var url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		prompt: 'consent',
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/calendar'
+			],
+			state: encodeURIComponent(JSON.stringify({
+				auth_id: req.query.auth_id
+			}))
+	});
+	res.redirect(url);})
 
 /*
 Create a POST route in the Express server and configure Slack 
